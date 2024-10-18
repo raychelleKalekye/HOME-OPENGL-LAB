11251d675da73e9b24bf19fda30de1d34ff4e51e
Raychelle Paul Kalekye <kalekyeraychelle@gmail.com>
2024-10-18 11:28:15 +0300

    moon drawn

--- home.py
+++ home.py
@@ -7,7 +7,19 @@ ctx = cairo.Context(surface)
 
 ctx.set_source_rgb(1, 1, 1)
 ctx.paint()
+moon_center_x = 900
+moon_center_y = 100
+moon_radius = 100
 
+
+ctx.set_source_rgb(0.5, 0.5, 0.5)
+ctx.arc(moon_center_x, moon_center_y, moon_radius, 0, 2 * math.pi)
+ctx.fill()
+
+
+ctx.set_source_rgb(1.0, 1.0, 1.0)
+ctx.arc(moon_center_x - 40, moon_center_y, moon_radius, 0, 2 * math.pi)
+ctx.fill()
 # house
 line_width = 5
 ctx.set_line_width(line_width)
